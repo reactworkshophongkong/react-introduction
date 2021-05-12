73365e8760bd1fb4e9c0ab477ae73a357bfabcb8
honchan <honkangchan@gmail.com>
2021-05-12 22:55:34 +0800

    add another useref example

--- src/hooks/App.js
+++ src/hooks/App.js
@@ -1,12 +1,16 @@
+import { useState } from 'react'
 import { useTrackClipboard, TrackClipboardClass } from './sharing-logic'
-// import { ExampleForUseRef } from './useref-example'
+import { DomElementExample, AsVariableExample } from './useref-example'
 
 function App() {
+  const [show, setShow] = useState(true)
   const data = useTrackClipboard()
 
   return (
     <>
-      <div className="App">
+      <p onClick={() => setShow(!show)}>Hide Element</p>
+      {show && <AsVariableExample />}
+      {/* <div className="App">
         <h1>Sharing Logic with hooks</h1>
         <p>You copied: <strong>{data}</strong></p>
       </div>
@@ -15,7 +19,7 @@ function App() {
           <h1>Sharing logic with class component via render props</h1>
           <p>You copied: <strong>{clipboardData}</strong></p>
         </div>
-      )}/>
+      )}/> */}
     </>
   )
 }
