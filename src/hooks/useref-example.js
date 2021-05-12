78c96160c4ca6525b07d6ead8426de318c1fe5fe
honchan <honkangchan@gmail.com>
2021-05-12 22:55:34 +0800

    add another useref example

--- src/hooks/useref-example.js
+++ src/hooks/useref-example.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 
 // refs allow us to get access to a DOM element
@@ -8,7 +8,7 @@ import React, { useEffect, useRef } from 'react'
 // the same and can assigned a new value
 
 
-const ExampleForUseRef = () => {
+const DomElementExample = () => {
   const inputRef = useRef(null)
 
   useEffect(() => {
@@ -24,4 +24,27 @@ const ExampleForUseRef = () => {
   )
 }
 
-export { ExampleForUseRef }
+// refs can also be used as instance variable in function component
+// here we do not want the component to rerender but we want to keep count of number of clicks
+const AsVariableExample = () => {
+  const clickCountRef = useRef(0)
+  const [count, setCount] = useState(0)
+
+  const handleClick = event => {
+    clickCountRef.current = clickCountRef.current + 1
+    // setCount(count + 1)
+  }
+
+  useEffect(() => {
+    return () => console.log('[AsVariableExample] rerendered clickCountRef.current', clickCountRef.current)
+  }, [])
+
+  console.log('[AsVariableExample] rerendered')
+
+  return (
+    <button onClick={handleClick}>Click me</button>
+  )
+}
+
+
+export { DomElementExample, AsVariableExample }
